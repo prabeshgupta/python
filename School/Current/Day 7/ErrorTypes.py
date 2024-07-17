86cb92e2198bd194e468a9f350a18febf5e9fe39
Prabesh Gupta <prabeshtech232@gmail.com>
2024-07-17 07:16:59 +0545

    Day 7

---
+++ School/Current/Day 7/ErrorTypes.py
@@ -0,0 +1,5 @@
+#Syntax Error -> Grammar Our program should be error free to be converted into byte code and executed.
+
+#Logical Error -> Program executes but Output is unexpected.
+
+#Runtime Error (Exception) -> Error encountered during execution. Program crashes unwantedly.
